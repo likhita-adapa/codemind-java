c4e0a2da3237fae987ed4058d36028710a08f7bb
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-11-26 21:09:34 +0530

    import java.util.Scanner; class Add {     public static void main(String args[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();
            System.out.println(a);
            sc.close();
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Add
+{
+    public static void main(String args[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+       
+        System.out.println(a);
+        sc.close();
+    }
+}

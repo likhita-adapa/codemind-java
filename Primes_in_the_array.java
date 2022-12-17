9e66937d1c47bacf52aebad8350f69ca0f44ba17
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-12-17 20:28:34 +0530

    import java.util.Scanner; import java.lang.Math; class palindrome {     static boolean prime(int x)     {         if (x<2)         {             return false;
            }
            for(int i=2;i<x;i++)
            {
                if(x%i==0)
                {
                    return false;
                    
                }
            }
            return true;
        }
        public static void main(String args[])
        {
            int n,count=0;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            int[] arr=new int[n]; 
            for(int i=0;i<n;i++)
            {
                arr[i]=sc.nextInt();
            }
            for(int i=0;i<n;i++)
            {
                if(prime(arr[i]))
                {
                    count++;
                }
            }
            System.out.println(count);
            sc.close();
        }
    }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.lang.Math;
+class palindrome
+{
+    static boolean prime(int x)
+    {
+        if (x<2)
+        {
+            return false;
+            
+        }
+        for(int i=2;i<x;i++)
+        {
+            if(x%i==0)
+            {
+                return false;
+                
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        int n,count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n]; 
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(prime(arr[i]))
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
